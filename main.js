ff7817dde0a1cedabb3ac407d4d4a07492e2c9fa
anouar el barry <elbarryanwar37@gmail.com>
2024-11-27 22:51:35 +0100

    cerat functionality to desplay each player on fonction of it's position

--- main.js
+++ main.js
@@ -12,19 +12,128 @@ fetch("FUT-Champ-Ultimate-Team-Assets/players.json")
 .then(data => 
    { if(data){
     listPlayers = data.players;
-    console.log(listPlayers);
 }}
 )
 .catch(error =>  console.error(error))
 
 
+function ceratCard(player){
+        let nameSize = null;
+        if(player.name.length >= 18){
+            nameSize=0.4;
+        }
+        const playerCard = document.createElement("div");
+        if(player.position === "GK"){
+            playerCard.innerHTML = `
+            <div class="fut-player-card">
+        <!-- Player Card Top-->
+        <div class="player-card-top">
+          <div class="player-master-info">
+            <div class="player-rating"><span>${player.rating}</span></div>
+            <div class="player-position"><span>${player.position}</span></div>
+            <div class="player-nation"><img src="${player.flag}" alt="${player.nationality}" draggable="false"/></div>
+            <div class="player-club"><img src="${player.logo}" alt="Barcelona" draggable="false"/></div>
+          </div>
+          <div class="player-picture"><img src="${player.photo}" alt="${player.name}" draggable="false"/>
+            
+          </div>
+        </div>
+        <!-- Player Card Bottom-->
+        <div class="player-card-bottom">
+          <div class="player-info">
+            <!-- Player Name-->
+            <div class="player-name"><span>${player.name}</span></div>
+            <!-- Player Features-->
+            <div class="player-features">
+              <div class="player-features-col"><span>
+                  <div class="player-feature-value">${player.diving}</div>
+                  <div class="player-feature-title">DIV</div></span><span>
+                  <div class="player-feature-value">${player.handling}</div>
+                  <div class="player-feature-title">HAN</div></span><span>
+                  <div class="player-feature-value">${player.kicking}</div>
+                  <div class="player-feature-title">KIC</div></span></div>
+              <div class="player-features-col"><span>
+                  <div class="player-feature-value">${player.reflexes}</div>
+                  <div class="player-feature-title">REF</div></span><span>
+                  <div class="player-feature-value">${player.speed}</div>
+                  <div class="player-feature-title">SPD</div></span>
+                  <span>
+                  <div class="player-feature-value">${player.positioning}</div>
+                  <div class="player-feature-title">POS</div>
+                  </span>
+                  </div>
+            </div>
+          </div>
+        </div>
+        </div>
+            `;
+        }
+        else{
+            playerCard.innerHTML = `
+            <div class="fut-player-card">
+        <!-- Player Card Top-->
+        <div class="player-card-top">
+          <div class="player-master-info">
+            <div class="player-rating"><span>${player.rating}</span></div>
+            <div class="player-position"><span>${player.position}</span></div>
+            <div class="player-nation"><img src="${player.flag}" alt="${player.nationality}" draggable="false"/></div>
+            <div class="player-club"><img src="${player.logo}" alt="Barcelona" draggable="false"/></div>
+          </div>
+          <div class="player-picture"><img src="${player.photo}" alt="${player.name}" draggable="false"/>
+            
+          </div>
+        </div>
+        <!-- Player Card Bottom-->
+        <div class="player-card-bottom">
+          <div class="player-info">
+            <!-- Player Name-->
+            <div class="player-name"><span  style="font-size:${nameSize}rem ;">${player.name}</span></div>
+            <!-- Player Features-->
+            <div class="player-features">
+              <div class="player-features-col"><span>
+                  <div class="player-feature-value">${player.pace}</div>
+                  <div class="player-feature-title">PAC</div></span><span>
+                  <div class="player-feature-value">${player.shooting}</div>
+                  <div class="player-feature-title">SHO</div></span><span>
+                  <div class="player-feature-value">${player.passing}</div>
+                  <div class="player-feature-title">PAS</div></span></div>
+              <div class="player-features-col"><span>
+                  <div class="player-feature-value">${player.dribbling}</div>
+                  <div class="player-feature-title">DRI</div></span><span>
+                  <div class="player-feature-value">${player.defending}</div>
+                  <div class="player-feature-title">DEF</div></span>
+                  <span>
+                  <div class="player-feature-value">${player.physical}</div>
+                  <div class="player-feature-title">PHY</div>
+                  </span>
+                  </div>
+            </div>
+          </div>
+        </div>
+        </div>
+            `;
+        }
+       console.log(playerCard);
+       console.log(player.position);
+       return playerCard;
+}
+
 const playersBox = document.querySelectorAll(".fut-player-card");
+const modalBox = document.querySelector(".modalPlayer");
+const modalContainer = document.querySelector(".modal_players");
 playersBox.forEach(box => {
    const plus= box.querySelector(".fa-plus");
     box.addEventListener('click',() =>{
         const positionSelected = box.getAttribute('data-position');
         console.log(box);
         console.log(positionSelected);
+      const playerPosition = listPlayers.filter(player => player.position === positionSelected);
+      playerPosition.forEach(player => {
+        
+        modalBox.append(ceratCard(player));
+      })
+        modalContainer.classList.add("flex");
+        modalContainer.classList.remove("hidden");
         plus.classList.add("hidden");
     })
 })
